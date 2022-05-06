f7176d4ee4339344de17535e198574bb1cb6a1a4
Anna Meier-Wagner <103186634+anna-mei@users.noreply.github.com>
2022-05-06 09:39:07 +0200

    SPIELER.java added

---
+++ SPIELER.java
@@ -0,0 +1,25 @@
+
+/**
+ * Beschreiben Sie hier die Klasse SPIELER.
+ * 
+ * @author (Ihr Name) 
+ * @version (eine Versionsnummer oder ein Datum)
+ */
+public class SPIELER
+{
+    private String name;
+
+    /**
+     * Konstruktor für Objekte der Klasse SPIELER
+     */
+    public SPIELER(String nameNeu)
+    {
+       setName(nameNeu);
+    }
+
+    
+    public void setName(String nameNeu)
+    {
+       name = nameNeu;
+    }
+}
